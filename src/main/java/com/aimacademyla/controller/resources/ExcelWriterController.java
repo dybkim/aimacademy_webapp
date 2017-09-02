ad188d62d7f9faa64956d0b8fda088334aed17c2
David Kim <dybkim@gmail.com>
2017-09-01 20:19:09 -0700

    Added missing SpringMVC dependencies to Maven

--- src/main/java/com/aimacademyla/controller/resources/ExcelWriterController.java
+++ src/main/java/com/aimacademyla/controller/resources/ExcelWriterController.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 
