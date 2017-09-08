51477fa8970fa83bdd71d4f2fa7d919d1e38465d
David Kim <dybkim@gmail.com>
2017-09-07 20:45:29 -0700

    Modified start script for codedeploy to allow for the replacement of revisions

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,4 +1,19 @@
 # Set Java launch options
 
+CATALINA_HOME='/usr/share/tomcat8'
+TEMP_STAGING_DIR='/tmp/codedeploy-deployment-staging-area'
+WAR_STAGED_LOCATION="$TEMP_STAGING_DIR/webportal.war"
+
 set JAVA_OPTS='-Dspring.profiles.active=production -Duser.timezone=GMT'
+
+if [[ -f "$CATALINA_HOME/webapps/webportal.war ]]; then
+	rm $CATALINA_HOME/webapps/webportal.war
+fi
+
+if[[ -d "$CATALINA_HOME/webapps/webportal ]]; then
+	rm -rfv $CATALINA_HOME/webapps/webportal
+fi
+
+cp $WAR_STAGED_LOCATION $CATALINA_HOME/webapps/webportal.war
+
 service tomcat8 start
