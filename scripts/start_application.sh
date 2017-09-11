f2673ab9a18a3465b0218580e89c15a97111d4c8
David Kim <dybkim@gmail.com>
2017-09-11 15:11:32 -0700

    Modified startup script to paste META-INF and WEB-INF files in tomcat8 web directory to ensure that the proper files are in the web directories

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -18,5 +18,7 @@ mkdir $CATALINA_HOME/webapps/webportal
 mv $WAR_STAGED_LOCATION $CATALINA_HOME/webapps/webportal.war
 mv $TEMP_STAGING_DIR/META-INF/ $CATALINA_HOME/webapps/webportal/
 mv $TEMP_STAGING_DIR/WEB-INF/ $CATALINA_HOME/webapps/webportal/
+mv $TEMP_STAGING_DIR/classes/ $CATALINA_HOME/webapps/webportal/WEB-INF/
+mv $TEMP_STAGING_DIR/lib/ $CATALINA_HOME/webapps/webportal/WEB-INF/
 
 service tomcat8 start
