fc53911bc5fcf91513a2c60549a8ebeada2fb285
David Kim <dybkim@gmail.com>
2017-12-06 00:16:55 -0800

    Reimplemented wrappers as DTO's

--- src/main/java/com/aimacademyla/controller/UserSettingsController.java
+++ src/main/java/com/aimacademyla/controller/UserSettingsController.java
@@ -1,12 +1,10 @@
 package com.aimacademyla.controller;
 
 import com.aimacademyla.model.User;
-import com.aimacademyla.model.wrapper.NewPasswordFormWrapper;
+import com.aimacademyla.model.dto.NewPasswordFormDTO;
 import com.aimacademyla.service.AuthorityService;
 import com.aimacademyla.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -14,48 +12,45 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 @Controller
 @RequestMapping("/admin/settings")
 public class UserSettingsController {
 
-    private AuthorityService authorityService;
     private UserService userService;
 
     @Autowired
-    public UserSettingsController(AuthorityService authorityService,
-                                  UserService userService){
-        this.authorityService = authorityService;
+    public UserSettingsController(AuthorityService authorityService){
         this.userService = userService;
     }
 
     @RequestMapping()
     public String getSettings(Model model){
-        model.addAttribute("newPasswordFormWrapper", new NewPasswordFormWrapper());
+        model.addAttribute("newPasswordFormWrapper", new NewPasswordFormDTO());
         return "settings";
     }
 
     @RequestMapping(method= RequestMethod.POST)
-    public String setSettings(@ModelAttribute("newPasswordFormWrapper") NewPasswordFormWrapper newPasswordFormWrapper, final RedirectAttributes redirectAttributes){
+    public String setSettings(@ModelAttribute("newPasswordFormWrapper") NewPasswordFormDTO newPasswordFormDTO, final RedirectAttributes redirectAttributes){
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userService.get(userDetails.getUsername());
         String currentPassword = user.getPassword();
 
-        if(!newPasswordFormWrapper.getCurrentPassword().equals(currentPassword)){
+        if(!newPasswordFormDTO.getCurrentPassword().equals(currentPassword)){
             redirectAttributes.addFlashAttribute("currentPasswordErrorMessage", "Current Password is incorrect");
 
-            if(!newPasswordFormWrapper.getNewPassword().equals(newPasswordFormWrapper.getConfirmPassword()))
+            if(!newPasswordFormDTO.getNewPassword().equals(newPasswordFormDTO.getConfirmPassword()))
                 redirectAttributes.addFlashAttribute("confirmPasswordErrorMessage", "New Passwords did not match!");
 
             return "redirect:/admin/settings";
         }
 
-        if(!newPasswordFormWrapper.getNewPassword().equals(newPasswordFormWrapper.getConfirmPassword())){
+        if(!newPasswordFormDTO.getNewPassword().equals(newPasswordFormDTO.getConfirmPassword())){
             redirectAttributes.addFlashAttribute("confirmPasswordErrorMessage", "New Passwords did not match!");
             return "redirect:/admin/settings";
         }
 
-
-        user.setPassword(newPasswordFormWrapper.getNewPassword());
+        user.setPassword(newPasswordFormDTO.getNewPassword());
         user.setIsEnabled(true);
         userService.update(user);
         return "redirect:/admin/home";
