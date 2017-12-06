1019e4837170f8c12b3c4bb806f95de8b7650076
David Kim <dybkim@gmail.com>
2017-12-05 21:49:59 -0800

    Abstracted initialization of entities to Builders and Initializers

--- src/main/java/com/aimacademyla/controller/course/rest/CourseResources.java
+++ src/main/java/com/aimacademyla/controller/course/rest/CourseResources.java
@@ -1,23 +1,23 @@
 package com.aimacademyla.controller.course.rest;
 
-import com.aimacademyla.controller.GenericResponse;
+import com.aimacademyla.exception.NullEntityCollectionException;
 import com.aimacademyla.model.*;
-import com.aimacademyla.model.builder.impl.CourseRegistrationWrapperBuilder;
-import com.aimacademyla.model.composite.MemberCourseRegistrationPK;
-import com.aimacademyla.model.wrapper.CourseRegistrationWrapper;
-import com.aimacademyla.service.*;
-import com.aimacademyla.service.factory.ServiceFactory;
+import com.aimacademyla.model.builder.entity.MemberCourseRegistrationBuilder;
+import com.aimacademyla.model.response.GenericResponse;
+import com.aimacademyla.service.CourseService;
+import com.aimacademyla.service.CourseSessionService;
+import com.aimacademyla.service.MemberCourseRegistrationService;
+import com.aimacademyla.service.MemberService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -27,38 +27,31 @@ import java.util.List;
 @RequestMapping("/admin/courseList/rest")
 public class CourseResources {
 
-    private ServiceFactory serviceFactory;
-
     private MemberService memberService;
+    private CourseSessionService courseSessionService;
     private CourseService courseService;
     private MemberCourseRegistrationService memberCourseRegistrationService;
-    private AttendanceService attendanceService;
+
+    private static final Logger logger = LogManager.getLogger(CourseResources.class.getName());
 
     @Autowired
-    public CourseResources(ServiceFactory serviceFactory,
-                           MemberService memberService,
+    public CourseResources(MemberService memberService,
                            CourseService courseService,
-                           MemberCourseRegistrationService memberCourseRegistrationService,
-                           AttendanceService attendanceService){
-        this.serviceFactory = serviceFactory;
+                           CourseSessionService courseSessionService,
+                           MemberCourseRegistrationService memberCourseRegistrationService){
         this.memberService = memberService;
         this.courseService = courseService;
+        this.courseSessionService = courseSessionService;
         this.memberCourseRegistrationService = memberCourseRegistrationService;
-        this.attendanceService = attendanceService;
-    }
-
-    @RequestMapping(value="/{courseID}/getCourseRegistration")
-    @ResponseBody
-    public CourseRegistrationWrapper getCourseRegistrationWrapper(@PathVariable("courseID") int courseID){
-        return new CourseRegistrationWrapperBuilder(serviceFactory).setCourseID(courseID).build();
     }
 
     @RequestMapping(value="/{courseID}/validateAddCourseSession")
     @ResponseBody
     public ResponseEntity<GenericResponse> validateAddCourseSession(@PathVariable("courseID") int courseID){
         Course course = courseService.get(courseID);
-        List<Member> memberList = memberService.getActiveMembersByCourse(course);
-        if(memberList.size() == 0)
+        List<MemberCourseRegistration> memberCourseRegistrationList = new ArrayList<>(course.getMemberCourseRegistrationSet());
+
+        if(memberCourseRegistrationList.size() == 0)
             return new ResponseEntity<>(new GenericResponse("Unable to create course session", "Cannot add course sessions until members are registered to the course!",HttpStatus.INTERNAL_SERVER_ERROR.value()) {
             }, HttpStatus.INTERNAL_SERVER_ERROR);
 
@@ -69,22 +62,25 @@ public class CourseResources {
     @ResponseBody
     public ResponseEntity<GenericResponse> deleteCourse(@PathVariable("courseID") int courseID){
         Course course = courseService.get(courseID);
-        courseService.remove(course);
+        courseService.removeCourse(course);
 
         return new ResponseEntity<>(new GenericResponse("OK", "NO_ERROR", HttpStatus.OK.value()){},HttpStatus.OK);
     }
 
     @RequestMapping(value="/{courseID}/submitRegistrationList", method=RequestMethod.POST)
     public String submitRegistrationList(@PathVariable("courseID") int courseID, @RequestBody List<Integer> memberIDList){
+        Course course = courseService.get(courseID);
+
         for(Integer memberID : memberIDList){
-            MemberCourseRegistrationPK memberCourseRegistrationPK = new MemberCourseRegistrationPK(memberID, courseID);
-            MemberCourseRegistration memberCourseRegistration = new MemberCourseRegistration();
-            memberCourseRegistration.setMemberCourseRegistrationPK(memberCourseRegistrationPK);
-            memberCourseRegistration.setMemberID(memberID);
-            memberCourseRegistration.setCourseID(courseID);
-            memberCourseRegistration.setIsEnrolled(true);
-            memberCourseRegistration.setDateRegistered(LocalDate.now());
-            memberCourseRegistrationService.update(memberCourseRegistration);
+            Member member = memberService.get(memberID);
+            MemberCourseRegistration memberCourseRegistration = new MemberCourseRegistrationBuilder()
+                                                                    .setMember(member)
+                                                                    .setCourse(course)
+                                                                    .setIsEnrolled(true)
+                                                                    .setDateRegistered(LocalDate.now())
+                                                                    .build();
+
+            memberCourseRegistrationService.addMemberCourseRegistration(memberCourseRegistration);
         }
 
         return "redirect:/admin/courseList/courseInfo/" + courseID;
@@ -93,12 +89,12 @@ public class CourseResources {
     @RequestMapping(value="/getCourseSession/{courseSessionID}")
     @ResponseBody
     public List<Member> getCourseSession(@PathVariable("courseSessionID") int courseSessionID) {
-        List<Attendance> attendanceList = attendanceService.getAttendanceForCourseSession(courseSessionID);
+        CourseSession courseSession = courseSessionService.get(courseSessionID);
         List<Member> memberList = new ArrayList<>();
 
-        for(Attendance attendance : attendanceList){
+        for(Attendance attendance : courseSession.getAttendanceMap().values()){
             if(attendance.getWasPresent()) {
-                Member member = memberService.get(attendance.getMemberID());
+                Member member = attendance.getMember();
                 memberList.add(member);
             }
         }
