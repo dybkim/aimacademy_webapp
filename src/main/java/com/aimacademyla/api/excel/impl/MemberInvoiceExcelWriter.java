16be4b23cca15e6b1696ad3d63f3b1658b3ef6a7
David Kim <dybkim@gmail.com>
2018-02-27 20:49:58 -0800

    Added MemberInvoiceExcelWriter
    MemberInvoiceExcelWriter creates a spreadsheet with all charges for a Member for a specified time period.

--- src/main/java/com/aimacademyla/api/excel/impl/MemberInvoiceExcelWriter.java
+++ src/main/java/com/aimacademyla/api/excel/impl/MemberInvoiceExcelWriter.java
@@ -1,28 +1,25 @@
 package com.aimacademyla.api.excel.impl;
 
-import com.aimacademyla.api.excel.MSExcelWriter;
+import com.aimacademyla.api.excel.AbstractExcelWriter;
 import com.aimacademyla.model.Charge;
+import com.aimacademyla.model.ChargeLine;
 import com.aimacademyla.model.Member;
 import com.aimacademyla.model.dto.MemberChargesFinancesDTO;
-import org.apache.poi.openxml4j.opc.OPCPackage;
+import com.aimacademyla.model.dto.PeriodSummaryDTO;
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.view.document.AbstractXlsxView;
+import org.apache.poi.ss.util.CellUtil;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class MemberInvoiceExcelWriter extends MSExcelWriter{
+public class MemberInvoiceExcelWriter extends AbstractExcelWriter {
+
+    public static final String TEMPLATE_FILE_NAME = "Invoice_Template.xlsx";
+
+    private PeriodSummaryDTO.MemberSummary memberSummary;
 
     public MemberInvoiceExcelWriter(Workbook workbook){
         super(workbook);
@@ -30,35 +27,16 @@ public class MemberInvoiceExcelWriter extends MSExcelWriter{
 
     @Override
     public Workbook writeToWorkbook() {
-        return null;
-    }
-
-    protected void buildExcelDocument(Map<String, Object> map, Workbook workbook, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        String invoiceTemplatePath = getClass().getClassLoader().getResource("Invoice_Template.xlsx").getFile();
-        File invoiceTemplateFile = new File(invoiceTemplatePath);
-        FileInputStream inputStream = new FileInputStream(invoiceTemplateFile);
-        OPCPackage opcPackage = OPCPackage.open(inputStream);
-
-        workbook = new XSSFWorkbook(opcPackage);
         Sheet invoiceSheet = workbook.getSheetAt(0);
+        Member member = memberSummary.getMember();
+        List<Charge> chargeList = new ArrayList<>(memberSummary.getChargeList());
 
-        MemberChargesFinancesDTO memberChargesFinancesDTO = (MemberChargesFinancesDTO) map.get("memberChargesFinancesDTO");
+        LocalDate cycleStartDate = memberSummary.getCyclePeriod().getCycleStartDate();
+        LocalDate cycleEndDate = memberSummary.getCyclePeriod().getCycleEndDate();
+        String dateString = cycleStartDate.getMonth().toString().substring(0,1) + cycleStartDate.getMonth().toString().substring(1).toLowerCase()
+                + " " + cycleStartDate.getDayOfMonth() + ", " + cycleStartDate.getYear() + " - " + cycleEndDate.getMonth().toString().substring(0,1) + cycleEndDate.getMonth().toString().substring(1).toLowerCase()
+                + " " + cycleEndDate.getDayOfMonth() + ", " + cycleEndDate.getYear();
 
-        Member member = memberChargesFinancesDTO.getMember();
-        LocalDate selectedDate = memberChargesFinancesDTO.getCycleStartDate();
-        List<Charge> chargeList = new ArrayList<>(memberChargesFinancesDTO.getChargeHashMap().values());
-        HashMap<Integer, BigDecimal> hoursBilledHashMap = memberChargesFinancesDTO.getBillableUnitsBilledHashMap();
-
-        String selectedMonthString = selectedDate.getMonth().toString().substring(0,1) + selectedDate.getMonth().toString().substring(1).toLowerCase();
-        String fileName = member.getMemberID() + "_" + selectedMonthString + selectedDate.getYear() + "_INVOICE.xlsx";
-
-        httpServletResponse.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=" + fileName);
-
-        FileOutputStream outputStream = new FileOutputStream("/Users/davidkim/IdeaProjects/webportal/target/webportal/WEB-INF/classes/new.xlsx");
-
-        String dateTodayString = LocalDate.now().getMonth().toString().substring(0,1) + LocalDate.now().getMonth().toString().substring(1).toLowerCase()
-                                    + " " + LocalDate.now().getDayOfMonth() + ", " + LocalDate.now().getYear();
         String invoiceNumberString = Integer.toString(LocalDate.now().getMonthValue()) + Integer.toString(LocalDate.now().getDayOfMonth()) + Integer.toString(LocalDate.now().getYear());
 
         Cell recipientCell = invoiceSheet.getRow(ExcelRowColumnWrapper.RECIPIENT_CELL.getRow()).getCell(ExcelRowColumnWrapper.RECIPIENT_CELL.getColumn());
@@ -67,53 +45,82 @@ public class MemberInvoiceExcelWriter extends MSExcelWriter{
         Cell invoiceNumberCell = invoiceSheet.getRow(ExcelRowColumnWrapper.INVOICE_NUMBER_CELL.getRow()).getCell(ExcelRowColumnWrapper.INVOICE_NUMBER_CELL.getColumn());
 
         recipientCell.setCellValue(member.getMemberFirstName() + " " + member.getMemberLastName());
-        dateCell.setCellValue(dateTodayString);
+        dateCell.setCellValue(dateString);
         studentIDCell.setCellValue(member.getMemberID());
         invoiceNumberCell.setCellValue(invoiceNumberString);
 
+        BigDecimal totalBalance = BigDecimal.ZERO;
+
         for(int count = 0; count < chargeList.size(); count++){
             Charge charge = chargeList.get(count);
-            String chargeDescription = charge.getDescription();
+
+            String chargeDescription = generateChargeDescription(charge);
+
             BigDecimal chargeAmount = charge.getChargeAmount();
             BigDecimal discountAmount = charge.getDiscountAmount();
-            BigDecimal hoursBilled = hoursBilledHashMap.get(charge.getChargeID());
+            BigDecimal hoursBilled = charge.getBillableUnitsBilled();
             BigDecimal balanceAmount = chargeAmount.subtract(discountAmount);
-
-            System.out.println(chargeDescription);
+            totalBalance = totalBalance.add(balanceAmount);
 
             Cell descriptionCell = invoiceSheet.getRow(ExcelRowColumnWrapper.DESCRIPTION_CELL_START.getRow() + count).getCell(ExcelRowColumnWrapper.DESCRIPTION_CELL_START.getColumn());
             Cell quantityCell = invoiceSheet.getRow(ExcelRowColumnWrapper.QUANTITY_CELL_START.getRow() + count).getCell(ExcelRowColumnWrapper.QUANTITY_CELL_START.getColumn());
             Cell priceCell = invoiceSheet.getRow(ExcelRowColumnWrapper.PRICE_CELL_START.getRow() + count).getCell(ExcelRowColumnWrapper.PRICE_CELL_START.getColumn());
             Cell discountCell = invoiceSheet.getRow(ExcelRowColumnWrapper.DISCOUNT_CELL_START.getRow() + count).getCell(ExcelRowColumnWrapper.DISCOUNT_CELL_START.getColumn());
-            Cell totalCell = invoiceSheet.getRow(ExcelRowColumnWrapper.TOTAL_CELL_START.getRow() + count).getCell(ExcelRowColumnWrapper.TOTAL_CELL_START.getColumn());
+            Cell subTotalCell = invoiceSheet.getRow(ExcelRowColumnWrapper.SUBTOTAL_CELL_START.getRow() + count).getCell(ExcelRowColumnWrapper.SUBTOTAL_CELL_START.getColumn());
 
             descriptionCell.setCellValue(chargeDescription);
             quantityCell.setCellValue(hoursBilled.toString());
             priceCell.setCellValue(chargeAmount.toString());
             discountCell.setCellValue(discountAmount.toString());
-            totalCell.setCellValue(balanceAmount.toString());
+            subTotalCell.setCellValue(balanceAmount.toString());
+
+            CellUtil.setAlignment(descriptionCell, HorizontalAlignment.CENTER);
+            CellUtil.setAlignment(quantityCell, HorizontalAlignment.CENTER);
+            CellUtil.setAlignment(priceCell, HorizontalAlignment.CENTER);
+            CellUtil.setAlignment(discountCell, HorizontalAlignment.CENTER);
+            CellUtil.setAlignment(subTotalCell, HorizontalAlignment.CENTER);
         }
 
-        workbook.write(outputStream);
-//        inputStream.close();
-        outputStream.close();
-        opcPackage.close();
+        Cell totalCell = invoiceSheet.getRow(ExcelRowColumnWrapper.TOTAL_CELL.getRow()).getCell(ExcelRowColumnWrapper.TOTAL_CELL.getColumn());
+        totalCell.setCellValue(totalBalance.toString());
+        CellUtil.setAlignment(totalCell, HorizontalAlignment.CENTER);
+
+        return workbook;
+    }
+
+    private String generateChargeDescription(Charge charge){
+        StringBuilder stringBuilder = new StringBuilder(charge.getDescription() + " (");
+        List<ChargeLine> chargeLineList = new ArrayList<>(charge.getChargeLineSet());
+
+        for(int i = 0; i < chargeLineList.size(); i++){
+            ChargeLine chargeLine = chargeLineList.get(i);
+            stringBuilder.append(chargeLine.getDateCharged().getMonthValue() + "/" + chargeLine.getDateCharged().getDayOfMonth() + "/" + chargeLine.getDateCharged().getYear());
+            if(i < chargeLineList.size() - 1)
+                stringBuilder.append(", ");
+        }
+        stringBuilder.append(")");
+        return stringBuilder.toString();
+    }
+
+    public MemberInvoiceExcelWriter setMemberSummary(PeriodSummaryDTO.MemberSummary memberSummary){
+        this.memberSummary = memberSummary;
+        return this;
     }
 
     private enum ExcelRowColumnWrapper{
-        /**
+        /*
          * NOTE: Excel cell index starts from 0
          */
         DESCRIPTION_CELL_START(19,0),
         QUANTITY_CELL_START(19,3),
         PRICE_CELL_START(19,4),
         DISCOUNT_CELL_START(19,5),
-        TOTAL_CELL_START(19,6),
+        SUBTOTAL_CELL_START(19,6),
         RECIPIENT_CELL(9,1),
         INVOICE_NUMBER_CELL(4,6),
         DATE_CELL(5,6),
-        STUDENT_ID_CELL(6,6);
-
+        STUDENT_ID_CELL(6,6),
+        TOTAL_CELL(39,6);
 
         private int column;
         private int row;
