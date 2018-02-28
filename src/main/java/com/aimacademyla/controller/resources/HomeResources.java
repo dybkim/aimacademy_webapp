2624283657f0781a89306b3a5e4851fdec66aa75
David Kim <dybkim@gmail.com>
2018-02-27 20:56:41 -0800

    Changed mapping to accept cycleStartDate and cycleEndDate to create a CyclePeriod required for OutstandingChargesPaymentDTO

--- src/main/java/com/aimacademyla/controller/resources/HomeResources.java
+++ src/main/java/com/aimacademyla/controller/resources/HomeResources.java
@@ -2,6 +2,8 @@ package com.aimacademyla.controller.resources;
 
 import com.aimacademyla.model.builder.dto.OutstandingChargesPaymentDTOBuilder;
 import com.aimacademyla.model.dto.OutstandingChargesPaymentDTO;
+import com.aimacademyla.model.temporal.CyclePeriod;
+import org.hibernate.type.LocalDateType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,10 +17,12 @@ public class HomeResources {
 
     @RequestMapping("/memberChargesList")
     @ResponseBody
-    public OutstandingChargesPaymentDTO getMemberChargesList(@RequestParam("month") int month,
-                                                             @RequestParam("year") int year){
-        LocalDate cycleStartDate = LocalDate.of(year, month, 1);
-        return new OutstandingChargesPaymentDTOBuilder().setCycleStartDate(cycleStartDate).build();
+    public OutstandingChargesPaymentDTO getMemberChargesList(@RequestParam("cycleStartDate") String cycleStartDateString,
+                                                             @RequestParam("cycleEndDate") String cycleEndDateString){
+        LocalDate cycleStartDate = LocalDate.parse(cycleStartDateString);
+        LocalDate cycleEndDate = LocalDate.parse(cycleEndDateString);
+        CyclePeriod cyclePeriod = new CyclePeriod(cycleStartDate, cycleEndDate);
+        return new OutstandingChargesPaymentDTOBuilder().setCyclePeriod(cyclePeriod).build();
     }
 }
 
